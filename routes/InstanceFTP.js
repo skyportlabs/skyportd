86653314392606d5ca04ca0f1e1343ffa121b051
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2024-08-08 23:54:21 -0300

    fix: now ftp connection can be done successfully

--- routes/InstanceFTP.js
+++ routes/InstanceFTP.js
@@ -15,30 +15,37 @@ const options = {
 const dataContainerDir = path.join(process.cwd(), 'ftp');
 const volumesDir = path.join(process.cwd(), 'volumes');
 
-const getDirectories = async (srcPath) => {
+const users = {};
+
+const getDirectories = async srcPath => {
   const files = await fs.readdir(srcPath);
-  const directories = await Promise.all(files.map(async file => {
-    const filePath = path.join(srcPath, file);
-    const stat = await fs.stat(filePath);
-    return stat.isDirectory() ? file : null;
-  }));
+  const directories = await Promise.all(
+    files.map(async file => {
+      const filePath = path.join(srcPath, file);
+      const stat = await fs.stat(filePath);
+      return stat.isDirectory() ? file : null;
+    }),
+  );
   return directories.filter(dir => dir !== null);
 };
 
-const generatePassword = (dirName) => {
+const generatePassword = dirName => {
   const sumOfDigits = dirName.split('').reduce((sum, char) => sum + (parseInt(char) || 0), 0);
   const randomNumber = Math.floor(Math.random() * 900) + 10;
   const otherRandomNumber = Math.floor(Math.random() * randomNumber) + 10;
-  const specialChars = "!@#$%&*_?~AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+  const specialChars = '!@#$%&*_?~AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
 
-  const replaceWithSpecialChar = () => specialChars[Math.floor(Math.random() * specialChars.length)];
-  
+  const replaceWithSpecialChar = () =>
+    specialChars[Math.floor(Math.random() * specialChars.length)];
+
   const replacedString = (sumOfDigits * Math.floor(randomNumber / otherRandomNumber))
     .toString()
     .replace(/[02481]/g, replaceWithSpecialChar);
 
-  const finalPassword = `${randomNumber}${replacedString}${randomNumber * Math.floor(randomNumber / otherRandomNumber)}`;
-  
+  const finalPassword = `${randomNumber}${replacedString}${
+    randomNumber * Math.floor(randomNumber / otherRandomNumber)
+  }`;
+
   return finalPassword.replace(/[24557]/g, replaceWithSpecialChar);
 };
 
@@ -50,9 +57,7 @@ const createUserData = (username, password, dir) => ({
   root: path.join(volumesDir, dir),
 });
 
-const users = {};
-
-const createNewVolume = async (dir) => {
+const createNewVolume = async dir => {
   const username = `user-${dir}`;
   const userFile = path.join(dataContainerDir, `${username}.json`);
 
@@ -80,38 +85,41 @@ const watchVolumesDirectory = () => {
 const initializeUsers = async () => {
   await fs.mkdir(dataContainerDir, { recursive: true });
   const directories = await getDirectories(volumesDir);
-  console.log('Directories found:', directories);
+  log.info('Directories found:', directories);
 
-  await Promise.all(directories.map(async (dir) => {
-    const username = `user-${dir}`;
-    const userFile = path.join(dataContainerDir, `${username}.json`);
+  await Promise.all(
+    directories.map(async dir => {
+      const username = `user-${dir}`;
+      const userFile = path.join(dataContainerDir, `${username}.json`);
 
-    let userData;
-    try {
-      userData = JSON.parse(await fs.readFile(userFile, 'utf8'));
-      users[username] = { password: userData.password, root: userData.root };
-      log.info(`User ${username} loaded with password: ${userData.password}`);
-    } catch (err) {
-      console.error(`Error reading or parsing ${userFile}: ${err.message}`);
-      const password = generatePassword(dir);
-      userData = createUserData(username, password, dir);
-      await fs.writeFile(userFile, JSON.stringify(userData, null, 2));
-      users[username] = { password, root: userData.root };
-      log.info(`User ${username} created with password: ${password}`);
-    }
-  }));
+      let userData;
+      try {
+        userData = JSON.parse(await fs.readFile(userFile, 'utf8'));
+        users[username] = { password: userData.password, root: userData.root };
+        log.info(`User ${username} loaded with password: ${userData.password}`);
+      } catch (err) {
+        log.error(`Error reading or parsing ${userFile}: ${err.message}`);
+        const password = generatePassword(dir);
+        userData = createUserData(username, password, dir);
+        await fs.writeFile(userFile, JSON.stringify(userData, null, 2));
+        users[username] = { password, root: userData.root };
+        log.info(`User ${username} created with password: ${password}`);
+      }
+    }),
+  );
 
   log.info('All FTP users are initialized');
 };
-
-initializeUsers();
 
 const createServer = () => {
   const server = new ftpd.FtpServer(options.host, {
     getInitialCwd: () => '/',
     getRoot: (connection, callback) => {
       const user = users[connection.username];
-      user ? callback(null, user.root) : callback(new Error('No such user'));
+
+      if (callback) {
+        user ? callback(null, user.root) : callback(new Error('No such user'), '/');
+      }
     },
     pasvPortRangeStart: 1025,
     pasvPortRangeEnd: 1050,
@@ -119,13 +127,12 @@ const createServer = () => {
     allowUnauthorizedTls: true,
     useWriteFile: false,
     useReadFile: false,
-    uploadMaxSlurpSize: 7000,
-    allowedCommands: ['XMKD', 'AUTH', 'TLS', 'SSL', 'USER', 'PASS', 'PWD', 'OPTS', 'TYPE', 'PORT', 'PASV', 'LIST', 'CWD', 'MKD', 'SIZE', 'STOR', 'MDTM', 'DELE', 'QUIT', 'RMD'],
+    uploadMaxSlurpSize: 20000, // 20 MB
   });
 
-  server.on('error', (error) => log.error('FTP Server error:', error));
+  server.on('error', error => log.error('FTP Server error:', error));
 
-  server.on('client:connected', (connection) => {
+  server.on('client:connected', connection => {
     let currentUser = null;
 
     connection.on('command:user', (user, success, failure) => {
